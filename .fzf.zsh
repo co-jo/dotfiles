77c20a3eada05d254989e85143cca6809ecc28b9
co-jo <colin.hryniowsk@dell.com>
2021-04-29 18:17:44 +0000

    Make USER parametric

--- .fzf.zsh
+++ .fzf.zsh
@@ -1,13 +1,13 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */home/colin/.fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}/home/colin/.fzf/bin"
+if [[ ! "$PATH" == */home/$USER/.fzf/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}/home/$USER/.fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/home/colin/.fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "/home/$USER/.fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/home/colin/.fzf/shell/key-bindings.zsh"
+source "/home/$USER/.fzf/shell/key-bindings.zsh"
--- update.sh
+++ update.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 HOME=$HOME
-USER=$(id -g -n)
+USER=${USER:-$(id -g -n)}
 GROUP=$(id -g)
+
+if [ $USER = 'root' ]; then
+    read -p "Using USER 'root', are you sure? (Press ENTER to continue)"
+fi
 
 list=(
 	'.config'
@@ -11,7 +15,12 @@ list=(
 	'.zshrc'
 )
 
+echo "Updating config files. Found:"
+echo -e "\tUSER:  $USER"
+echo -e "\tHOME:  $HOME"
+echo -e "\tGROUP: $GROUP"
+
 for file in ${list[@]}; do
-	sudo cp -r $file $HOME
-	sudo chown -R $USER:$GROUP $HOME/$file
+	cp -r $file $HOME
+	chown -R $USER:$GROUP $HOME/$file
 done
